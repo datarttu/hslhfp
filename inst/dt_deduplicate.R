7dc7aa3c8a0e3e66be401ad93afb0c95fe5ae226
keripukki <arttu.kosonen@gmail.com>
2021-04-13 14:23:08 +0300

    Refactor dt_deduplicate.R
    - Process all .csv.gz files of a dir
    - Emit one log row per file

--- inst/dt_deduplicate.R
+++ inst/dt_deduplicate.R
@@ -1,48 +1,85 @@
 #!/usr/bin/env Rscript
 #'
-#' Sort and deduplicate contents of a route_dir_oday csv file.
+#' Sort and deduplicate route_dir_oday.csv.gz files.
 #' Deduplication is done based on ENTIRE duplicate rows.
 #'
 #' Usage: either
-#' > ./dt_deduplicate.R <input_file.csv.gz> <output_file.csv.gz>
+#' > ./dt_deduplicate.R <input_dir> <output_dir>
 #' OR
-#' > ./dt_deduplicate.R <input_output_file.csv.gz>
-#' In the latter, the input file is overwritten as output file.
+#' > ./dt_deduplicate.R <input_output_dir>
+#' In the latter, the input files are overwritten as output files.
 
 suppressMessages(library(data.table))
+suppressMessages(library(purrr))
 
 #' This is needed for .csv.gz reading with fread to work
 stopifnot('R.utils' %in% installed.packages())
 
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) == 0) {
-  stop('Usage: ./dt_deduplicate.R <input_file> [<output_file>]')
+  stop('Usage: ./dt_deduplicate.R <input_dir> [<output_dir>]')
 }
-INPUT_FILE <- args[1]
-stopifnot(file.exists(INPUT_FILE))
-OUTPUT_FILE <- ifelse(length(args) > 1, args[2], INPUT_FILE)
+INPUT_DIR <- args[1]
+stopifnot(dir.exists(INPUT_DIR))
+OUTPUT_DIR <- ifelse(length(args) > 1, args[2], INPUT_DIR)
+stopifnot(dir.exists(OUTPUT_DIR))
 
 tmessage <- function(...) {
   message('[', Sys.time(), ']; deduplicate; ', ...)
 }
 
-tmessage('Reading ', INPUT_FILE)
+deduplicate_file <- function(infile, outfile, file_seq = NA, total_files = NA) {
+  progmeter <- ifelse(
+    !is.na(file_seq) & !is.na(total_files),
+    sprintf('%04d/%d', file_seq, total_files),
+    ''
+  )
+  dt <- fread(file = infile)
+  nrow_orig <- nrow(dt)
+  if (nrow_orig == 0) {
+    tmessage(
+      sprintf(
+        '%d/%d; no rows in %s, skipping',
+        file_seq, total_files, infile
+      )
+    )
+    return()
+  }
 
-dt <- fread(file = INPUT_FILE)
-nrow_orig <- nrow(dt)
-if (nrow_orig == 0) {
-  tmessage('No rows in original file, exiting')
-  stop(call. = FALSE)
+  dt_dedup <- unique(
+    dt[order(route, dir, oday, start, oper, veh, tst, event_type), ]
+  )
+  nrow_dedup <- nrow(dt_dedup)
+  n_duplicated <- nrow_orig - nrow_dedup
+
+  fwrite(x = dt_dedup, file = outfile)
+
+  tmessage(
+    sprintf(
+      '%s; %d duplicates removed from %s and %d rows saved to %s',
+      progmeter, n_duplicated, infile, nrow_dedup, outfile
+    )
+  )
 }
-tmessage(nrow(dt), ' rows in original file')
 
-dt_dedup <- unique(
-  dt[order(route, dir, oday, start, oper, veh, tst, event_type), ]
+file_paths <- data.frame(
+  infile = list.files(path = INPUT_DIR, pattern = '*.csv.gz',
+                      full.names = TRUE),
+  stringsAsFactors = FALSE
 )
-nrow_dedup <- nrow(dt_dedup)
-n_duplicated <- nrow_orig - nrow_dedup
+file_paths$outfile <- gsub(
+  pattern = INPUT_DIR,
+  replacement = OUTPUT_DIR,
+  x = file_paths$infile
+)
+file_paths$file_seq <- seq_along(file_paths$infile)
+file_paths$total_files <- nrow(file_paths)
 
-tmessage(n_duplicated, ' duplicate rows removed')
+tmessage('Starting deduplication for ',
+         nrow(file_paths),
+         ' files')
 
-fwrite(x = dt_dedup, file = OUTPUT_FILE)
-tmessage(nrow_dedup, ' rows written to ', OUTPUT_FILE)
+foo <- purrr::pwalk(.l = file_paths,
+                    .f = deduplicate_file)
+
+tmessage('All files filtered')
